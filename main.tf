cc8d066f6e0e301607e089c0ea0dd785c3c25d3d
Tess DiStefano <tess.distefano@gmail.com>
2021-04-21 17:29:21 -0700

    upgrade

--- main.tf
+++ main.tf
@@ -1,9 +1,12 @@
-provider "azurerm" {
-  # The "feature" block is required for AzureRM provider 2.x.
-  # If you're using version 1.x, the "features" block is not allowed.
-  version = "~>2.0"
-  features {}
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.52.0"
+    }
+  }
 }
+
 
 resource "azurerm_resource_group" "myterraformgroup" {
   name     = "vm-test-boa-dev"
@@ -12,6 +15,11 @@ resource "azurerm_resource_group" "myterraformgroup" {
   tags = {
     environment = "Terraform Demo"
   }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
 }
 
 resource "azurerm_virtual_network" "myterraformnetwork" {
